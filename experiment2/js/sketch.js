e01d4b69c85cc6ca8f59f157042111fdf263f1b8
WontonsofDMG <mitchelltai221@gmail.com>
2025-01-20 17:14:20 -0800

    Implemented the project

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,79 +1,141 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+// Constants
+const INITIAL_CIRCLE_SIZE = 50;
+const INITIAL_ERASE_SPEED = 8;
+const INITIAL_SHAKE_AMOUNT = 5;
+const INITIAL_RED_INCREMENT_FACTOR = 10;
+const RED_THRESHOLD = 255;
 
 // Globals
-let myInstance;
 let canvasContainer;
-var centerHorz, centerVert;
+let circleInstance;
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
+// Circle class to encapsulate circle behavior
+class Circle {
+  constructor(x, y, size, eraseSpeed, shakeAmount, redIncrementFactor) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.eraseSpeed = eraseSpeed;
+    this.shakeAmount = shakeAmount;
+    this.redIncrementFactor = redIncrementFactor;
+    this.redLevel = 0;
+    this.drawingIntensity = 0;
+    this.erasing = false;
+    this.allErased = false;
+  }
+
+  drawCircle() {
+    if (!this.erasing) {
+      fill(255, 255 - this.redLevel, 255 - this.redLevel);
+      stroke(0);
+      strokeWeight(1);
+      ellipse(this.x, this.y, this.size);
+
+      if (this.redLevel >= RED_THRESHOLD) {
+        this.erasing = true;
+      }
+    }
+  }
+
+  handleDrawing(mouseX, mouseY, pmouseX, pmouseY) {
+    stroke(0);
+    strokeWeight(4);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+
+    this.drawingIntensity += dist(mouseX, mouseY, pmouseX, pmouseY);
+    this.redLevel = constrain(this.drawingIntensity / this.redIncrementFactor, 0, 255);
+  }
+
+  chaseAndErase() {
+    loadPixels();
+    let closestDist = Infinity;
+    let targetX = this.x;
+    let targetY = this.y;
+
+    for (let x = 0; x < width; x++) {
+      for (let y = 0; y < height; y++) {
+        let index = (x + y * width) * 4;
+        let r = pixels[index];
+        let g = pixels[index + 1];
+        let b = pixels[index + 2];
+
+        if (r === 0 && g === 0 && b === 0) {
+          let d = dist(this.x, this.y, x, y);
+          if (d < closestDist) {
+            closestDist = d;
+            targetX = x;
+            targetY = y;
+          }
+        }
+      }
     }
 
-    myMethod() {
-        // code to run when method is called
+    if (closestDist < Infinity) {
+      let angle = atan2(targetY - this.y, targetX - this.x);
+      let prevX = this.x;
+      let prevY = this.y;
+
+      this.x += cos(angle) * this.eraseSpeed + random(-this.shakeAmount, this.shakeAmount);
+      this.y += sin(angle) * this.eraseSpeed + random(-this.shakeAmount, this.shakeAmount);
+
+      noStroke();
+      fill(255);
+      ellipse(prevX, prevY, this.size);
+
+      fill(255, 0, 0);
+      stroke(255);
+      strokeWeight(2);
+      ellipse(this.x, this.y, this.size);
+    } else {
+      this.allErased = true;
     }
+
+    if (this.allErased) {
+      this.resetAfterErasing();
+    }
+  }
+
+  resetAfterErasing() {
+    this.erasing = false;
+    this.allErased = false;
+
+    this.eraseSpeed += 1;
+    this.shakeAmount += 2;
+    if (this.redIncrementFactor > 1) {
+      this.redIncrementFactor -= 1;
+    }
+
+    this.drawingIntensity = 0;
+    this.redLevel = 0;
+    this.x = this.size / 2 + 10;
+    this.y = height - this.size / 2 - 10;
+
+    background(255);
+  }
 }
 
 function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-  console.log("Resizing...");
   resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
 }
 
-// setup() function is called once when the program starts
 function setup() {
-  // place our canvas, making it fit our container
   canvasContainer = $("#canvas-container");
   let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
   canvas.parent("canvas-container");
-  // resize canvas is the page is resized
 
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
+  circleInstance = new Circle(
+    INITIAL_CIRCLE_SIZE / 2 + 10,
+    height - INITIAL_CIRCLE_SIZE / 2 - 10,
+    INITIAL_CIRCLE_SIZE,
+    INITIAL_ERASE_SPEED,
+    INITIAL_SHAKE_AMOUNT,
+    INITIAL_RED_INCREMENT_FACTOR
+  );
 
-  $(window).resize(function() {
-    resizeScreen();
-  });
+  $(window).resize(resizeScreen);
   resizeScreen();
 }
 
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function mouseDragged() {
+  circleInstance.handleDrawing(mouseX, mouseY, pmouseX, pmouseY);
 }
