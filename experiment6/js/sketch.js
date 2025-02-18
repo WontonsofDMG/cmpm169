f8a8ed8dcb6f0adc2f2125a49b8e2e0ba16c8d9d
WontonsofDMG <mitchelltai221@gmail.com>
2025-02-18 11:04:20 -0800

    lowered engine sound

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -38,7 +38,7 @@ function setup() {
   startButton.mousePressed(startGame);
 }
 function startGame() {
-  engineSound.setVolume(0.3);
+  engineSound.setVolume(0.25);
   engineSound.loop();
   gameStarted = true;
   startButton.hide();
